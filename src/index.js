4af1a0a5fc683043bcbd4916631ea894d284fe94
mrbrunor <contato@brunoricardo.com.br>
2021-12-07 16:35:27 -0300

    Desafio concluido

--- src/index.js
+++ src/index.js
@@ -10,19 +10,73 @@ app.use(cors());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+
+  const user = users.find((user) => user.username = username);
+
+  if(!user){
+    return response.status(404).json({error: "User not found!"});
+  }
+
+  request.user = user;  
+
+  return next();
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
-  // Complete aqui
+  const { user } = request;  
+
+  let amountTodos = 0;
+
+  user.todos.forEach(element => {
+    amountTodos += 1;
+  });
+
+  if (user.pro){
+    return next();
+  } else if(!user.pro && amountTodos < 10){
+    return next();
+  } else {
+    return response.status(403).json();
+  }
 }
 
 function checksTodoExists(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+  const { id } = request.params;
+
+  const user = users.find((user) => user.username = username);
+
+  if(!validate(id, 4)){
+    return response.status(400).json({error: "Id is not an uuid!"});
+  }
+
+  if(!user){
+    return response.status(404).json({error: "User not found!"});
+  }
+
+  const todo = user.todos.find((todo) => todo.id = id);
+
+  if(!todo){
+    return response.status(404).json({error: "Todo not found!"});
+  }
+
+  request.user = user;
+  request.todo = todo;
+  return next();
 }
 
 function findUserById(request, response, next) {
-  // Complete aqui
+  const { id } = request.params;
+
+  const user = users.find((user) => user.id = id);
+
+  if(!user){
+    return response.status(404).json({error: "User not found!"});
+  }
+
+  request.user = user;  
+  return next();
 }
 
 app.post('/users', (request, response) => {
